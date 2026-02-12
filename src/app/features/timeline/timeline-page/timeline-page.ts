d4d3585c8e02eceb052b7d8e9110e5458ffa89b2
Leah Mendoza <leahmedina.lm@gmail.com>
2026-02-12 09:19:04 -0600

    add timeline feature structure

---
+++ src/app/features/timeline/timeline-page/timeline-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timeline-page',
+  imports: [],
+  templateUrl: './timeline-page.html',
+  styleUrl: './timeline-page.scss',
+})
+export class TimelinePage {
+
+}
